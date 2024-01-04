8f9bfb7016d619884bfef25c713821e0625e7950
Nict <71667497+NictX@users.noreply.github.com>
2024-01-04 09:39:50 +0300

    Update streamrunner.py

--- src/streamlink_cli/streamrunner.py
+++ src/streamlink_cli/streamrunner.py
@@ -134,7 +134,7 @@ class StreamRunner:
                         print(f"New file must be created, part - {n}")
                         data_len=0
                         self.output.close()
-                        self.output = FileOutput(filename=Path(os.path.realpath(f"check{n}.ts")))
+                        self.output = FileOutput(filename=Path(os.path.realpath(formatter.path(f"check{n}.ts", args.fs_safe_rules))))
                         self.output.open()
                         write = self.output.write
                     if data == b"":
