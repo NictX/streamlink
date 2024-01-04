fec3b134c93119c4b384fbd7537c4f06da7b8d0c
Nict <71667497+NictX@users.noreply.github.com>
2024-01-04 09:36:48 +0300

    Update streamrunner.py

--- src/streamlink_cli/streamrunner.py
+++ src/streamlink_cli/streamrunner.py
@@ -134,7 +134,7 @@ class StreamRunner:
                         print(f"New file must be created, part - {n}")
                         data_len=0
                         self.output.close()
-                        self.output = FileOutput(filename=f"/home/nict/check{n}.ts")
+                        self.output = FileOutput(filename=Path(os.path.realpath(f"check{n}.ts")))
                         self.output.open()
                         write = self.output.write
                     if data == b"":
